ca61408e63bc64e711ab19b4e2aacef15190c101
afjoseph <7126721+afjoseph@users.noreply.github.com>
2021-08-17 17:27:26 +0200

    [run_avd] Add api_type and make emulators RW by default

--- misc/run_avd.sh
+++ misc/run_avd.sh
@@ -49,6 +49,7 @@ on_exit() {
 create_test_avd() {
     local -r android_api_level=$1
     local -r android_abi=$2
+    local -r api_type=$3
     local target_avd_image=""
 
     log_info "Checking AVDs..."
@@ -64,7 +65,7 @@ create_test_avd() {
         exit "$EXIT_FAIL"
     fi
 
-    target_avd_image="system-images;android-$android_api_level;default;$android_abi"
+    target_avd_image="system-images;android-$android_api_level;$api_type;$android_abi"
     if ! "$BIN_AVDMANAGER" --silent list avds 1>/dev/null | grep -q "$TEST_AVD_NAME"; then
         log_info "Could not find AVD. Creating $TEST_AVD_NAME ..."
 
@@ -82,10 +83,10 @@ launch_avd() {
     # `emulator` must be an absolute path to the bin
     if [ "$IS_HEADLESS" = true ]; then
         log_info "Launching headless emulator [$TEST_AVD_NAME] ..."
-        "$BIN_EMULATOR" "@$TEST_AVD_NAME" -no-audio -no-window -no-snapshot -wipe-data &
+        "$BIN_EMULATOR" "@$TEST_AVD_NAME" -writable-system -no-audio -no-window -no-snapshot -wipe-data &
     else
         log_info "Launching emulator with UI [$TEST_AVD_NAME] ..."
-        "$BIN_EMULATOR" "@$TEST_AVD_NAME" -no-snapshot -wipe-data &
+        "$BIN_EMULATOR" "@$TEST_AVD_NAME" -writable-system -no-snapshot -wipe-data &
     fi
 
     log_info "Waiting until emulator goes online..."
@@ -165,11 +166,16 @@ kill_avds() {
 main() {
     local android_api_level=""
     local android_abi="x86"
+    local api_type="default"
 
     for i in "$@"; do
         case $i in
             --headless)
                 IS_HEADLESS=true
+                shift
+                ;;
+            --use_google_apis)
+                api_type='google_apis'
                 shift
                 ;;
             --android_api_level=*)
@@ -194,7 +200,7 @@ main() {
 
     kill_avds
 
-    create_test_avd "$android_api_level" "$android_abi"
+    create_test_avd "$android_api_level" "$android_abi" "$api_type"
 
     launch_avd
 
